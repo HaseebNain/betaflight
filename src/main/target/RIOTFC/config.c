f9be64c61da25f6bd520779df7ec013fbff6c9d4
Haseeb Nain <haseebnain@u.boisestate.edu>
2017-08-01 14:28:55 -0600

    Readded Base RiotFC Target

---
+++ src/main/target/RIOTFC/config.c
@@ -0,0 +1,195 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <platform.h>
+
+
+
+#include "build/build_config.h"
+#include "build/debug.h"
+
+#include "blackbox/blackbox_io.h"
+
+#include "cms/cms.h"
+
+#include "common/axis.h"
+#include "common/color.h"
+#include "common/filter.h"
+#include "common/maths.h"
+
+#include "config/config_eeprom.h"
+#include "config/feature.h"
+#include "config/parameter_group.h"
+#include "config/parameter_group_ids.h"
+
+#include "drivers/accgyro/accgyro.h"
+#include "drivers/compass/compass.h"
+#include "drivers/io.h"
+#include "drivers/light_led.h"
+#include "drivers/light_ws2811strip.h"
+#include "drivers/max7456.h"
+#include "drivers/pwm_esc_detect.h"
+#include "drivers/pwm_output.h"
+#include "drivers/rx_pwm.h"
+#include "drivers/rx_spi.h"
+#include "drivers/sdcard.h"
+#include "drivers/sensor.h"
+#include "drivers/serial.h"
+#include "drivers/sonar_hcsr04.h"
+#include "drivers/sound_beeper.h"
+#include "drivers/system.h"
+#include "drivers/timer.h"
+#include "drivers/vcd.h"
+
+#include "fc/config.h"
+#include "fc/controlrate_profile.h"
+#include "fc/fc_core.h"
+#include "fc/fc_rc.h"
+#include "fc/rc_adjustments.h"
+#include "fc/rc_controls.h"
+#include "fc/runtime_config.h"
+
+#include "flight/altitude.h"
+#include "flight/failsafe.h"
+#include "flight/imu.h"
+#include "flight/mixer.h"
+#include "flight/navigation.h"
+#include "flight/pid.h"
+#include "flight/servos.h"
+
+#include "io/beeper.h"
+#include "io/gimbal.h"
+#include "io/gps.h"
+#include "io/ledstrip.h"
+#include "io/motors.h"
+#include "io/osd.h"
+#include "io/serial.h"
+#include "io/servos.h"
+#include "io/vtx_control.h"
+
+#include "rx/rx.h"
+#include "rx/rx_spi.h"
+
+#include "sensors/acceleration.h"
+#include "sensors/barometer.h"
+#include "sensors/battery.h"
+#include "sensors/boardalignment.h"
+#include "sensors/compass.h"
+#include "sensors/gyro.h"
+#include "sensors/sensors.h"
+
+#include "telemetry/telemetry.h"
+
+
+// Default Settings for the RiotFC rev 1.802c
+void targetConfiguration(void)
+{
+	// Motor Configurations
+	// 3 = PWM_TYPE_MULTISHOT
+	motorConfigMutable()->dev.motorPwmProtocol = 3;
+	motorConfigMutable()->minthrottle = 1040;
+	
+	//Smartport Configurations
+	//telemetryConfigMutable()->dev.telemetry_inversion = 0;
+	//telemetryConfigMutable()->dev.sportHalfDuplex = 0;
+	
+	// Barometer Configuration
+	//config->barometerConfig.baro_hardware = 3;
+
+	
+	// Motor Board Allignment Configurations
+    boardAlignmentMutable()->rollDegrees = 0;
+	boardAlignmentMutable()->pitchDegrees = 180;
+	boardAlignmentMutable()->yawDegrees = 270;
+	
+	// Battery Configurations
+	voltageSensorADCConfigMutable(VOLTAGE_SENSOR_ADC_VBAT)->vbatscale = 53;
+	currentSensorADCConfigMutable()->scale = 250;
+	
+	// PID Configuration set to 8KHz
+	pidConfigMutable()->pid_process_denom = 1;
+
+	
+	
+	// PID Profiile Configurations
+	
+	pidProfilesMutable(0)->P8[ROLL] = 43;
+	pidProfilesMutable(0)->I8[ROLL] = 50;
+	pidProfilesMutable(0)->D8[ROLL] = 20;
+	pidProfilesMutable(0)->P8[PITCH] = 60;
+	pidProfilesMutable(0)->I8[PITCH] = 55;
+	pidProfilesMutable(0)->D8[PITCH] = 19;
+	pidProfilesMutable(0)->P8[YAW] = 80;
+	pidProfilesMutable(0)->I8[YAW] = 55;
+	pidProfilesMutable(0)->D8[YAW] = 20;
+	pidProfilesMutable(0)->P8[PIDALT] = 50;
+	pidProfilesMutable(0)->I8[PIDALT] = 0;
+	pidProfilesMutable(0)->D8[PIDALT] = 0;
+	pidProfilesMutable(0)->P8[PIDPOS] = 15;   
+	pidProfilesMutable(0)->I8[PIDPOS] = 0;    
+	pidProfilesMutable(0)->D8[PIDPOS] = 0;
+	pidProfilesMutable(0)->P8[PIDPOSR] = 34;  
+	pidProfilesMutable(0)->I8[PIDPOSR] = 14;  
+	pidProfilesMutable(0)->D8[PIDPOSR] = 53;  
+	pidProfilesMutable(0)->P8[PIDNAVR] = 25;  
+	pidProfilesMutable(0)->I8[PIDNAVR] = 33;  
+	pidProfilesMutable(0)->D8[PIDNAVR] = 83;  
+	pidProfilesMutable(0)->P8[PIDLEVEL] = 50;
+	pidProfilesMutable(0)->I8[PIDLEVEL] = 50;
+	pidProfilesMutable(0)->D8[PIDLEVEL] = 100;
+	pidProfilesMutable(0)->P8[PIDMAG] = 40;
+	pidProfilesMutable(0)->P8[PIDVEL] = 55;
+	pidProfilesMutable(0)->I8[PIDVEL] = 55;
+	pidProfilesMutable(0)->D8[PIDVEL] = 75;
+
+	
+	// Preset Port assignments
+	serialConfigMutable()->portConfigs[0].identifier = serialPortIdentifiers[0];
+	serialConfigMutable()->portConfigs[0].msp_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[0].gps_baudrateIndex = BAUD_57600;
+	serialConfigMutable()->portConfigs[0].telemetry_baudrateIndex = BAUD_AUTO;
+	serialConfigMutable()->portConfigs[0].blackbox_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[0].functionMask = FUNCTION_MSP;
+	
+	serialConfigMutable()->portConfigs[1].identifier = serialPortIdentifiers[1];
+	serialConfigMutable()->portConfigs[1].msp_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[1].gps_baudrateIndex = BAUD_57600;
+	serialConfigMutable()->portConfigs[1].telemetry_baudrateIndex = BAUD_AUTO;
+	serialConfigMutable()->portConfigs[1].blackbox_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[1].functionMask = FUNCTION_MSP;
+	
+	serialConfigMutable()->portConfigs[2].identifier = serialPortIdentifiers[2];
+	serialConfigMutable()->portConfigs[2].msp_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[2].gps_baudrateIndex = BAUD_57600;
+	serialConfigMutable()->portConfigs[2].telemetry_baudrateIndex = BAUD_57600;
+	serialConfigMutable()->portConfigs[2].blackbox_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[2].functionMask = FUNCTION_TELEMETRY_SMARTPORT;
+	
+	serialConfigMutable()->portConfigs[3].identifier = serialPortIdentifiers[3];
+	serialConfigMutable()->portConfigs[3].msp_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[3].gps_baudrateIndex = BAUD_57600;
+	serialConfigMutable()->portConfigs[3].telemetry_baudrateIndex = BAUD_AUTO;
+	serialConfigMutable()->portConfigs[3].blackbox_baudrateIndex = BAUD_115200;
+	serialConfigMutable()->portConfigs[3].functionMask = FUNCTION_RX_SERIAL;
+	
+
+}
+
+
---
+++ src/main/target/RIOTFC/target.h
@@ -0,0 +1,202 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#define TARGET_BOARD_IDENTIFIER "RIOTFC"
+#define TARGET_CONFIG
+
+#define CONFIG_START_FLASH_ADDRESS 0x08080000 //0x08080000 to 0x080A0000 (FLASH_Sector_8)
+
+#define USBD_PRODUCT_STRING "Riot FC"
+/*
+#ifdef OPBL
+	#define USBD_SERIALNUMBER_STRING "0x8020000"
+#endif
+*/
+
+/*	LED Pin Configuration	*/
+#define LED0                    PB5
+#define LED1                    PB4
+#define LED2                    PB6
+
+
+#define INVERTER_PIN_USART6 	PC6
+#define INVERTER_USART          USART6
+
+// MPU9250 interrupt
+// Need to test and see if neccessary
+#define USE_EXTI
+#define MPU_INT_EXTI            PC5
+#define EXTI_CALLBACK_HANDLER_COUNT 1 // MPU data ready
+//#define DEBUG_MPU_DATA_READY_INTERRUPT
+#define USE_MPU_DATA_READY_SIGNAL
+#define ENSURE_MPU_DATA_READY_IS_LOW
+
+/*	Legacy ACC/GYRO Configuration	*/
+/*	MPU6500 ACC/GYRO				*/
+/*
+
+#define MPU6500_CS_PIN          PC4
+#define MPU6500_SPI_INSTANCE    SPI1
+
+#define ACC
+#define USE_ACC_SPI_MPU6500
+#define ACC_MPU6500_ALIGN       CW270_DEG
+
+#define GYRO
+#define defaultRoll		0
+#define defaultPitch	180
+#define defaultYaw		270
+#define USE_GYRO_SPI_MPU6500
+#define GYRO_MPU6500_ALIGN      CW270_DEG
+ 
+*/ 
+
+
+/*	MPU6000 Configuration	*/
+#define MPU6000_CS_PIN          PC4
+#define MPU6000_SPI_INSTANCE    SPI1
+
+#define ACC
+#define USE_ACC_SPI_MPU6000
+#define ACC_MPU6000_ALIGN       CW270_DEG
+
+#define GYRO
+#define USE_GYRO_SPI_MPU6000
+#define GYRO_MPU6000_ALIGN      CW270_DEG
+
+/*	Preset Roll/Pitch/Yaw
+#define defaultRoll		0
+#define defaultPitch	180
+#define defaultYaw		270
+*/
+
+
+/*	Barometer Configuration	*/
+#define BARO
+#define USE_BARO_MS5611
+#define MS5611_I2C_INSTANCE I2CDEV_1
+
+
+/*	SD CARD Configuration Information	*/
+#define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
+#define USE_SDCARD
+
+#define SDCARD_SPI_INSTANCE                 SPI3
+#define SDCARD_SPI_CS_PIN                   SPI3_NSS_PIN
+
+// SPI2 is on the APB1 bus whose clock runs at 84MHz. Divide to under 400kHz for init:
+#define SDCARD_SPI_INITIALIZATION_CLOCK_DIVIDER 256 // 328kHz
+// Divide to under 25MHz for normal operation:
+#define SDCARD_SPI_FULL_SPEED_CLOCK_DIVIDER     4 // 21MHz
+
+#define SDCARD_DMA_CHANNEL_TX               DMA1_Stream5
+#define SDCARD_DMA_CHANNEL_TX_COMPLETE_FLAG DMA_FLAG_TCIF5
+#define SDCARD_DMA_CLK                      RCC_AHB1Periph_DMA1
+#define SDCARD_DMA_CHANNEL                  DMA_Channel_0
+
+/*	UART Configuration	*/
+#define USE_VCP
+#define VBUS_SENSING_PIN        PA8
+
+#define USE_UART1
+#define UART1_RX_PIN            PA10
+#define UART1_TX_PIN            PA9
+#define UART1_AHB1_PERIPHERALS  RCC_AHB1Periph_DMA2
+
+#define USE_UART3
+#define UART3_RX_PIN            PB11
+#define UART3_TX_PIN            PB10
+
+#define USE_UART6
+#define UART6_RX_PIN            PC7
+#define UART6_TX_PIN            PC6 //inverter
+
+#define SERIAL_PORT_COUNT 4
+
+
+/*	SPI Devices	*/
+#define USE_SPI
+
+#define USE_SPI_DEVICE_1 
+#define SPI1_NSS_PIN            PC4
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
+#define USE_SPI_DEVICE_3 // SDCard SPI
+#define SPI3_NSS_PIN            PB3
+#define SPI3_SCK_PIN            PC10
+#define SPI3_MISO_PIN           PC11
+#define SPI3_MOSI_PIN           PC12
+
+/*	I2C Devices	*/
+#define USE_I2C
+#define I2C_DEVICE              (I2CDEV_1)
+//#define I2C_DEVICE_EXT          (I2CDEV_2)
+
+///	MS5611 I2C Pins
+#define I2C1_SCL                PB8
+#define I2C1_SDA                PB9
+
+/*	ADC Configuration	*/
+#define USE_ADC
+#define VBAT_SCALE_DEFAULT      53 
+#define BOARD_HAS_VOLTAGE_DIVIDER
+
+#define CURRENT_METER_ADC_PIN       PC2
+#define CURRENT_METER_ADC_CHANNEL   ADC_Channel_12
+
+#define VBAT_ADC_PIN           PC3
+#define VBAT_ADC_CHANNEL       ADC_Channel_13
+
+
+/*	LED Strip Driver Configuration	*/
+#define LED_STRIP
+#define LED_STRIP_LENGTH 35
+#define RIOTLEDCONFIG
+
+/*	Legacy WS2811 Configuration Information */
+/*
+#define WS2811_PIN                      PA0
+#define WS2811_TIMER                    TIM5
+#define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
+#define WS2811_DMA_STREAM               DMA1_Stream2
+#define WS2811_DMA_IT                   DMA_IT_TCIF2
+#define WS2811_DMA_CHANNEL              DMA_Channel_6
+#define WS2811_TIMER_CHANNEL            TIM_Channel_1
+*/
+
+/* Default Configuration Information	*/	
+#define TELEMETRY
+#define DEFAULT_FEATURES        (FEATURE_LED_STRIP | FEATURE_AIRMODE | FEATURE_TELEMETRY)
+#define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define SERIALRX_UART           SERIAL_PORT_USART6
+#define RX_CHANNELS_TAER
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
+/*	Timer Configuration	*/
+
+#define TARGET_IO_PORTA 0xffff
+#define TARGET_IO_PORTB 0xffff
+#define TARGET_IO_PORTC 0xffff
+
+
+#define USABLE_TIMER_CHANNEL_COUNT 11
+#define USED_TIMERS  ( TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(12) | TIM_N(8) | TIM_N(9))
+
